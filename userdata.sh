b7bb28066213b588cb43abf7d86968d7c9eb218a
vinodkumar <vinod199733@gmail.com>
2023-04-28 13:07:04 +0530

    Auto commit

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U https://github.com/sriharitirumala/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/mannepallyvinod/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
